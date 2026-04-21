1e1a50765f626f8af8f75c96f91254be36f8c277
Khaled <khaled-marai1@hotmail.com>
2026-04-21 20:53:06 +0300

    Fix Express app structure and separate server/app/router files

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import api from './api/index.js';
+import express from 'express';
+
+const app = express(); // FIRST create app
+
+app.use(cors()); //  THEN use it
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/uploads', express.static('uploads'));
+
+app.use('/api/v1', api);
+
+app.get('/', (req, res) => {
+  res.send('🔥 NightWolf API running on port!');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.json({ok: true, data: req.body});
+});
+
+export default app;
